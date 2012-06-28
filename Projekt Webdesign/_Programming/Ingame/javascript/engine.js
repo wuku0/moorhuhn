8e8e48a57a414f9dcd44468baa7e5ca6b292f8ee
sebastian.goeltz@gmail.com <sebastian.goeltz@gmail.com@9fe0734a-6c3e-dd9d-8da0-862f739c2ed6>
2012-06-28 16:31:12 +0000

    neuer Code, aber noch buggi. Falls aiming Methode verwenden wird, gehen die Abschüsse nicht mehr. Zudem lagt es teilweiße sehr.
    Eingebaut:
    Time
    Score
    Positionsabfrage des Cursors
    Neue Bilder

--- Projekt Webdesign/_Programming/Ingame/javascript/engine.js
+++ Projekt Webdesign/_Programming/Ingame/javascript/engine.js
@@ -14,6 +14,8 @@ function main () {
 	setposY();
 	var drawIntervalId = window.setInterval(drawchicken,10);
 	
+	
+	
 	$("#r1").click(function(){$(this).fadeOut(0)});
 	$("#r2").click(function(){$(this).fadeOut(0)});
 	$("#r3").click(function(){$(this).fadeOut(0)});
@@ -34,9 +36,33 @@ function createYvalue() {
 	return Math.round((((Math.random()*Math.random())*1000)%400));
 }
 
+function startPosition_x(){
+	if(Math.round((Math.random()*1000)%2) == 1)
+	{
+		$(".chickenLeft").css("left", Math.round((Math.random()*(-1000))%(-150)));
+	}
+	else
+	{
+		$(".chickenRight").css("right", Math.round((Math.random()*(-1000))%(-150)));
+	}
+}
+
+function startPosition_y(){
+	return Math.round((((Math.random()*Math.random())*1000)%400));
+}
+
 function drawchicken() {
 	$("#r1").css("left","+=0.8");
 	$("#r2").css("left","+=1.0");
 	$("#r3").css("left","+=1.2");
 	$("#r4").css("left","+=1.4");
+	
+}
+
+function aiming() {
+	$("#window").mousemove(function(e){
+		$("#crosshair").css("left", e.pageX-300);
+		$("#crosshair").css("top", e.pageY-134);
+		$('#status').html(e.pageX +', '+ e.pageY);
+	});
 }
